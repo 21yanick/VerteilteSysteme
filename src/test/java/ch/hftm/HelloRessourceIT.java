30a39b343ec3d35e1e0931e4d819da157702a2d8
21yanick <yanick.rupp@hftm.ch>
2024-04-26 15:40:20 +0200

    Neu initialisierung Quarkus Projekt

---
+++ src/test/java/ch/hftm/HelloRessourceIT.java
@@ -0,0 +1,8 @@
+package ch.hftm;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class HelloRessourceIT extends HelloRessourceTest {
+    // Execute the same tests but in packaged mode.
+}
